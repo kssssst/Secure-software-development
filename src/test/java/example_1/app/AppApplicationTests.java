75e53f0fb0a323cbe69b9cf1521e6e28a9f7e08e
Ксения <ksu@MacBook-Air-Ksenia.local>
2025-10-10 13:37:41 +0300

    Initial commit: Spring Boot project working

---
+++ src/test/java/example_1/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package example_1.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
